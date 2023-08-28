52d3188225fac837812fd4442d23248a0fc3f975
Vlad Nichifor <vlad.nichifor@cst.ro>
2023-08-28 16:33:09 +0300

    init

---
+++ src/test/java/com/tyme/wizards/WizardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyme.wizards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
