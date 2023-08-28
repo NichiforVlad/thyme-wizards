64117468285d939831233d59a2cdf4916917e391
Vlad Nichifor <vlad.nichifor@cst.ro>
2023-08-28 16:44:32 +0300

    build index controller

---
+++ src/main/java/com/tyme/wizards/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.tyme.wizards.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "ThymeWizards");
+        model.addAttribute("pageAuthors", List.of("Vlad", "Vladus", "Vlodimir"));
+        return "index";
+    }
+
+}
